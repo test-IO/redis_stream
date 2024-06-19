13358d35e393422e0602ea7e00c66d761c279aa3
Garcia Fabien <fab0670312047@gmail.com>
2024-06-19 17:53:02 +0200

    Remove

--- lib/redis_stream/publisher.rb
+++ lib/redis_stream/publisher.rb
@@ -9,7 +9,6 @@ module RedisStream
     end
 
     def publish(name, data = {})
-      puts "Publishing #{name} to #{data}"
       data = {"name" => name, "json" => JSON.generate(data)}
       RedisStream.client.xadd(@stream_key, data)
     end
