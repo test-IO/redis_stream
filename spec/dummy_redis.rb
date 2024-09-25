590bb2ac268da012cc8c5939606b23dd8ca5d703
Garcia Fabien <fab0670312047@gmail.com>
2024-09-25 10:32:36 +0200

    [TEST] Ensure test are checked on pull request (#7)
    * [TEST] Ensure test are checked on pull request
    Run test automatically on push
    
    * fix gemfile lock
    
    * fix test
    
    * Fix test

--- spec/dummy_redis.rb
+++ spec/dummy_redis.rb
@@ -8,7 +8,7 @@ class DummyRedisClient
     @streams[stream] << data
   end
 
-  def xreadgroup(group, consumer, streams, condition, count: 1, block: 5000)
+  def xreadgroup(group, consumer, streams, condition, count: 1, block: 5000, noack: false)
     @streams[streams] ||= []
     @streams[streams].map.with_index do |data, index|
       [streams, {index => data}]
