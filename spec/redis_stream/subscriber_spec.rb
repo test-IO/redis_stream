eca10b8c5ca8a173a822644a79757f5d29d684d3
aleksei-okatiev <64004628+aleksei-okatiev@users.noreply.github.com>
2024-11-12 20:15:35 +0100

    added loop to the listen method

--- spec/redis_stream/subscriber_spec.rb
+++ spec/redis_stream/subscriber_spec.rb
@@ -9,12 +9,24 @@ RSpec.describe RedisStream::Subscriber do
 
   describe ".listen" do
     it "subscribes to the stream" do
-      described_class.listen(streams: "stream_test") do |message|
-        expect(message).to eq("test")
+      allow(described_class).to receive(:loop).and_yield.twice
+
+      allow(RedisStream.client).to receive(:xreadgroup).and_return([
+        ["stream_test", [["message_id", {"name" => "test", "json" => "{}"}]]]
+      ])
+
+      described_class.listen(streams: "stream_test") do |stream, message_id, name, json|
+        expect(stream).to eq("stream_test")
+        expect(message_id).to eq("message_id")
+        expect(name).to eq("test")
+        expect(json).to eq({})
       end
 
-      described_class.listen(streams: ["stream_test"]) do |message|
-        expect(message).to eq("test")
+      described_class.listen(streams: ["stream_test"]) do |stream, message_id, name, json|
+        expect(stream).to eq("stream_test")
+        expect(message_id).to eq("message_id")
+        expect(name).to eq("test")
+        expect(json).to eq({})
       end
     end
   end
